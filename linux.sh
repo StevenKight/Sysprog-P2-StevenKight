77c1abcc9c833a40575c8e0aeca594d559b64626
StevenKight <sgk0711@gmail.com>
2023-10-30 13:27:29 -0400

    Implement search for blacklisted IPs.

--- linux.sh
+++ linux.sh
@@ -5,9 +5,23 @@ file_name="access.log"
 file_id="1Wuko3rkpL9TtOXTSIVmZ3tFHYFQs2sxf"
 
 curl -L -o $file_name "https://drive.google.com/uc?export=download&id={$file_id}"
+echo -e "Downloaded log file: $file_name\n"
 
 # Read blacklisted IPs from file
 blacklist_file="./blacklist.txt"
 readarray -t blacklisted < $blacklist_file
+echo -e "Blacklisted IPs loaded from file: $blacklist_file\n"
 
-echo "Blacklisted IPs: ${blacklisted[@]}"
+# Read log file line by line
+line_number=1
+while read -r line; 
+do
+    for ip in "${blacklisted[@]}"
+    do
+        if [[ $line == *"$ip"* ]]; then
+            echo "Blacklisted IP: $ip found in log file on line: $line_number"
+        fi
+    done
+
+    ((line_number++))
+done < "$file_name"
