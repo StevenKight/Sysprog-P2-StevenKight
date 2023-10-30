0690b9e6cd8fd38003cee2858ca7eb7c840a43c8
StevenKight <sgk0711@gmail.com>
2023-10-30 13:12:38 -0400

    Implement list of blacklisted IPs in a text file.

--- linux.sh
+++ linux.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Download log file
 file_name="access.log"
 file_id="1Wuko3rkpL9TtOXTSIVmZ3tFHYFQs2sxf"
 
 curl -L -o $file_name "https://drive.google.com/uc?export=download&id={$file_id}"
+
+# Read blacklisted IPs from file
+blacklist_file="./blacklist.txt"
+readarray -t blacklisted < $blacklist_file
+
+echo "Blacklisted IPs: ${blacklisted[@]}"
