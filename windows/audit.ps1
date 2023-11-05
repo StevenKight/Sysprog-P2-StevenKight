3c857b37aa7c342d8d02b860056afad0a7b37644
StevenKight <sgk0711@gmail.com>
2023-11-04 20:22:14 -0400

    Implement ip and sql checking in powershell.

--- windows/audit.ps1
+++ windows/audit.ps1
@@ -13,6 +13,17 @@ $WebClient.DownloadFile($Url, ".\$filename")
 Write-Host "Downloaded $filename from Google Drive"
 Write-Host
 
+# Load list of blacklisted IPs
+$blacklist = Get-Content "./blacklist.txt"
+
+# Load SQL injection patterns
+$sqlInjection = @(
+    "UNION",
+    "SELECT",
+    "' OR 1=1",
+    "DROP TABLE"
+)
+
 # Read log file line by line
 $lines = Get-Content ${FILENAME}
 
@@ -23,7 +34,19 @@ $line_number = 0
 while ($line_number -lt $lines.Length) {
     $line = $lines[$line_number]
 
-    Write-Host "Line $line_number: $line"
+    # Check if the line contains a blacklisted IP
+    foreach ($ip in $blacklist) {
+        if ($line.Contains($ip)) {
+            Write-Host "Found blacklisted IP: $ip"
+        }
+    }
+
+    # Check if the line contains a SQL injection string
+    foreach ($pattern in $sqlInjection) {
+        if ($line.Contains($pattern)) {
+            Write-Host "Found SQL injection: $pattern"
+        }
+    }
 
     $line_number++
 }
