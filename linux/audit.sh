ce570e610efac6216a32b23d4cfd2a75e715ef40
StevenKight <sgk0711@gmail.com>
2023-11-04 20:18:28 -0400

    Move linux script into seperate folder.

--- linux/audit.sh
+++ linux/audit.sh
@@ -20,23 +20,33 @@ sql_injection_patterns=(
     "DROP TABLE"
 )
 
-# Read log file line by line
+# Setup output file
+output_file="audit_rpt.txt"
+if [ -f "audit_rpt.txt" ]; then # TODO: Should file be cleared?
+    > "$output_file"
+else
+    touch "audit_rpt.txt"
+fi
+echo -e "Output file prepared: $output_file\n"
+
+# Read log file line by line and audit
 line_number=1
 while read -r line; 
 do
     for ip in "${blacklisted[@]}"
     do
         if [[ $line == *"$ip"* ]]; then
-            echo "Blacklisted IP: $ip found in log file on line: $line_number"
+            echo "$line_number, Found blacklisted IP: $ip" >> "$output_file"
         fi
     done
 
     for pattern in "${sql_injection_patterns[@]}"
     do
         if [[ $line == *"$pattern"* ]]; then
-            echo "SQL injection pattern: $pattern found in log file on line: $line_number"
+            echo "$line_number, Found SQL injection pattern: $pattern" >> "$output_file"
         fi
     done
 
     ((line_number++))
 done < "$file_name"
+echo -e "Audit completed. Output written to $output_file\n"
